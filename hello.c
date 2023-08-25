d4071e455318cf8c342b64200764102664d6ba90
ksonne2 <ksonne1@huskers.unl.edu>
2023-08-25 11:00:40 -0700

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Kyle Sonne
+ *         EMAIL
+ * Date: 20xx/xx/xx
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello my name is Kyle Sonne!!\n");
+  printf("And I love the School of Computing!\n");
+
+  return 0;
+}
